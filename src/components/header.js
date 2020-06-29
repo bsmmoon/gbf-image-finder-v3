f8ff08a8ebdd8c0790ea1a11511bb77c10436745
brian.moon <bsmmoon@gmail.com>
2020-06-29 11:20:48 +0000

    Added react-bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -28,6 +28,12 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+/>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Button from "react-bootstrap/Button"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <Button>HI</Button>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -20,3 +23,4 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
