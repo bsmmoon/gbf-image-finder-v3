91d94392344679abc63637d4f335207c17f46bf1
bsmmoon <bsmmoon@gmail.com>
2021-03-28 06:20:59 +0000

    install fontawesome

---
+++ src/helpers/fontawesome.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClipboard } from '@fortawesome/free-regular-svg-icons'
+
+export const clipboard = () => <FontAwesomeIcon icon={faClipboard} />
+
